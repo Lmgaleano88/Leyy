865f4c54ca1e7787db0192b8ddc84b738f76d530
User <User@LEYDI22S>
2019-05-13 13:02:35 -0500

    Incompleto

---
+++ Juego/Juego/Juego/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Juego
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            incio.Clicked += incio_Clicked;
+            Crear.Clicked += crear_Clicked;
+        }
+        private void incio_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Inicio());
+        }
+        private void crear_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new registro());
+        }
+    }
+}
