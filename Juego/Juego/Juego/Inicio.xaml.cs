aabfd9da1b133dda1932d256cf38acfc74008a90
User <User@LEYDI22S>
2019-06-18 16:52:16 -0500

    ok

--- Juego/Juego/Juego/Inicio.xaml.cs
+++ Juego/Juego/Juego/Inicio.xaml.cs
@@ -15,6 +15,30 @@ namespace Juego
 		public Inicio ()
 		{
 			InitializeComponent ();
+            registrar.Clicked += registra_Clicked;
+            visualizar.Clicked += visualizar_Clicked;
+            Informacion.Clicked += info_Clicked;
+            Licencias.Clicked += licen_Clicked;
+
 		}
-	}
+        private void registra_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new registarjuego());
+        }
+        private void visualizar_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new visualizarjuego());
+        }
+        private void info_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new informaciondejuego());
+        }
+        private void licen_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new informaciondejuego());
+        }
+
+
+
+    }
 }
---
+++ Juego/Juego/Juego/visualizarjuego.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Juego
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class visualizarjuego : ContentPage
+	{
+		public visualizarjuego ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
